505051a356e06bc4a468d1ca1a3bd0923755f8fe
DylanDr1 <dylan.dosramos@ontariotechu.net>
2024-11-12 14:51:01 -0500

    Update1

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+  
+    public static Timer Instance { get; private set; }
+
+ 
+    [SerializeField] private TextMeshProUGUI timeText;
+
+ 
+    private float elapsedTime;
+    private bool isRunning;
+
+   
+
+    private void Start()
+    {
+        StartTimer();
+    }
+
+    private void Update()
+    {
+        if (isRunning)
+        {
+            elapsedTime += Time.deltaTime;
+            UpdateTimeDisplay();
+        }
+    }
+
+    public void StartTimer()
+    {
+        isRunning = true;
+    }
+
+    public void StopTimer()
+    {
+        isRunning = false;
+    }
+
+    public void ResetTimer()
+    {
+        elapsedTime = 0;
+        UpdateTimeDisplay();
+    }
+
+    private void UpdateTimeDisplay()
+    {
+      
+        int minutes = Mathf.FloorToInt(elapsedTime / 60F);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60F);
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
+
